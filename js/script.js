df0cfb320ae83668882ca4ebabbba4b15f6e0e81
mehak <mehakrani adamjee26@gmail.com>
2023-06-06 17:01:24 +0500

    hi

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function print1()
+{
+    var one  = document.getElementById("box1").value
+     var package1 =  one*7
+     document.getElementById("paragraph1").innerHTML = "$" + package1
+   
+}
+function print2()
+{
+    var two  = document.getElementById("box2").value
+    var package2 =  two*15
+    document.getElementById("paragraph2").innerHTML = "$" + package2
+}
+
+function print3()
+{
+    var three = document.getElementById("box3").value
+    var package3 = three*25
+    document.getElementById("paragraph3").innerHTML = "$" + package3
+}
+
+function print4()
+{
+    var four  = document.getElementById("box4").value
+    var package4 = four*20
+    document.getElementById("paragraph4").innerHTML = "$" + package4
+
+}
+
+function print5()
+{
+    var five  = document.getElementById("box5").value
+    var package5 =five*20
+  document.getElementById("paragraph5").innerHTML = "$" + package5
+}  
+
+function print6()
+{
+    var six  = document.getElementById("box6").value
+    var package6 =six*7
+  document.getElementById("paragraph6").innerHTML = "$" + package6
+}  
+
+function print7()
+{
+    var seven = document.getElementById("box7").value
+    var package7 =seven*20
+  document.getElementById("paragraph7").innerHTML = "$" + package7
+} 
+
+function print8()
+{
+    var eight  = document.getElementById("box8").value
+    var package8 =eight*10
+  document.getElementById("paragraph8").innerHTML = "$" + package8
+}  
+
+function print9()
+{
+    var nine  = document.getElementById("box9").value
+    var package9 =nine*5
+  document.getElementById("paragraph9").innerHTML = "$" + package9
+}  
+
+
+
+
+// whats app icon code
